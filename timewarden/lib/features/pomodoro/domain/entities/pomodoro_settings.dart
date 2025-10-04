962687fb99e17260889a47a989038682d5f2dfb6
“PavanZensark” <pavan.royyala@zensark.com>
2025-10-04 19:12:44 +0530

    statstics issues

--- timewarden/lib/features/pomodoro/domain/entities/pomodoro_settings.dart
+++ timewarden/lib/features/pomodoro/domain/entities/pomodoro_settings.dart
@@ -5,48 +5,31 @@ class PomodoroSettings extends Equatable {
   final int shortBreakMinutes;
   final int longBreakMinutes;
   final int sessionsUntilLongBreak;
-  final bool autoStartBreaks;
-  final bool autoStartWork;
   final bool enableNotifications;
   final bool enableSounds;
   final bool enableVibration;
   final double soundVolume;
-  final String selectedSound;
 
   const PomodoroSettings({
     this.workDurationMinutes = 25,
     this.shortBreakMinutes = 5,
     this.longBreakMinutes = 15,
     this.sessionsUntilLongBreak = 4,
-    this.autoStartBreaks = false,
-    this.autoStartWork = false,
     this.enableNotifications = true,
     this.enableSounds = true,
     this.enableVibration = true,
     this.soundVolume = 0.8,
-    this.selectedSound = 'bell',
   });
-
-  static const List<String> availableSounds = [
-    'bell',
-    'chime',
-    'ding',
-    'notification',
-    'beep',
-  ];
 
   PomodoroSettings copyWith({
     int? workDurationMinutes,
     int? shortBreakMinutes,
     int? longBreakMinutes,
     int? sessionsUntilLongBreak,
-    bool? autoStartBreaks,
-    bool? autoStartWork,
     bool? enableNotifications,
     bool? enableSounds,
     bool? enableVibration,
     double? soundVolume,
-    String? selectedSound,
   }) {
     return PomodoroSettings(
       workDurationMinutes: workDurationMinutes ?? this.workDurationMinutes,
@@ -54,13 +37,10 @@ class PomodoroSettings extends Equatable {
       longBreakMinutes: longBreakMinutes ?? this.longBreakMinutes,
       sessionsUntilLongBreak:
           sessionsUntilLongBreak ?? this.sessionsUntilLongBreak,
-      autoStartBreaks: autoStartBreaks ?? this.autoStartBreaks,
-      autoStartWork: autoStartWork ?? this.autoStartWork,
       enableNotifications: enableNotifications ?? this.enableNotifications,
       enableSounds: enableSounds ?? this.enableSounds,
       enableVibration: enableVibration ?? this.enableVibration,
       soundVolume: soundVolume ?? this.soundVolume,
-      selectedSound: selectedSound ?? this.selectedSound,
     );
   }
 
@@ -70,13 +50,10 @@ class PomodoroSettings extends Equatable {
       'shortBreakMinutes': shortBreakMinutes,
       'longBreakMinutes': longBreakMinutes,
       'sessionsUntilLongBreak': sessionsUntilLongBreak,
-      'autoStartBreaks': autoStartBreaks,
-      'autoStartWork': autoStartWork,
       'enableNotifications': enableNotifications,
       'enableSounds': enableSounds,
       'enableVibration': enableVibration,
       'soundVolume': soundVolume,
-      'selectedSound': selectedSound,
     };
   }
 
@@ -86,13 +63,10 @@ class PomodoroSettings extends Equatable {
       shortBreakMinutes: json['shortBreakMinutes'] as int? ?? 5,
       longBreakMinutes: json['longBreakMinutes'] as int? ?? 15,
       sessionsUntilLongBreak: json['sessionsUntilLongBreak'] as int? ?? 4,
-      autoStartBreaks: json['autoStartBreaks'] as bool? ?? false,
-      autoStartWork: json['autoStartWork'] as bool? ?? false,
       enableNotifications: json['enableNotifications'] as bool? ?? true,
       enableSounds: json['enableSounds'] as bool? ?? true,
       enableVibration: json['enableVibration'] as bool? ?? true,
       soundVolume: (json['soundVolume'] as num?)?.toDouble() ?? 0.8,
-      selectedSound: json['selectedSound'] as String? ?? 'bell',
     );
   }
 
@@ -102,12 +76,9 @@ class PomodoroSettings extends Equatable {
         shortBreakMinutes,
         longBreakMinutes,
         sessionsUntilLongBreak,
-        autoStartBreaks,
-        autoStartWork,
         enableNotifications,
         enableSounds,
         enableVibration,
         soundVolume,
-        selectedSound,
       ];
 }
